970f484263c9e457b1360142d55b979bb29733b9
Sam Anthony <sam@samanthony.xyz>
2025-10-29 17:24:43 -0400

    whitebalance, gamma correction, and sharpening

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = g++
 INCLUDES = -I/usr/include/opencv4
 CFLAGS = -std=c++11 -Wall -pedantic -g ${INCLUDES}
-LDFLAGS = -lopencv_flann -lopencv_gapi -lopencv_highgui -lopencv_ml -lopencv_photo -lopencv_video -lopencv_dnn -lopencv_videoio -lopencv_imgcodecs -lopencv_imgproc -lopencv_core
+LDFLAGS = -lopencv_imgcodecs -lopencv_xphoto -lopencv_imgproc -lopencv_core
 
 SRC = underwater.cpp
 HDR = $(wildcard *.hpp)
--- underwater.cpp
+++ underwater.cpp
@@ -2,80 +2,155 @@
  * Ancuti et al. 2018
  */
 
+#include <cassert>
 #include <iostream>
 
+#include <stdint.h>
+
 #include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/xphoto/white_balance.hpp>
 
 using namespace std;
-using namespace cv;
 
 #define ALPHA 1.0 // red compensation factor
+#define GAMMA 0.8 // gamma correction
+#define GAUSS_KERN_SIZE cv::Size(5, 5) // Gaussian blur filter size
 
 typedef enum {
 	OK,
 	FAIL,
 } Status;
 
-// Read an image from disk and convert it to float64 normalized to [0, 1].
+// Read an image from disk and convert it CV_64FC3 BGR.
 static Status
-readImage(const string &path, Mat &img) {
-	img = imread(path, IMREAD_COLOR_BGR);
+readImage(const string &path, cv::Mat &img) {
+	// Read image
+	img = cv::imread(path, cv::IMREAD_COLOR_BGR);
 	if (img.empty()) {
 		return FAIL;
 	}
-	img.convertTo(img, CV_64FC3); // convert to float64
-	normalize(img, img, 1.0, 0.0, NORM_MINMAX); // normalize to [0, 1]
+
+	// Convert to float64
+	img.convertTo(img, CV_64FC3);
+	cv::normalize(img, img, 1.0, 0.0, cv::NORM_MINMAX);
+
 	return OK;
 }
 
-// Write to disk a float64 image normalized to [0, 1]
+// Write a CV_64FC3 BGR image to disk.
 static void
-writeImage(const string &path, const Mat &img) {
-	Mat tmp;
-	normalize(img, tmp, 255, 0, NORM_MINMAX); // normalize to [0,255]
+writeImage(const string &path, const cv::Mat &img) {
+	cv::Mat tmp;
+	cv::normalize(img, tmp, 255, 0, cv::NORM_MINMAX);
 	tmp.convertTo(tmp, CV_8UC3);
 	imwrite(path, tmp);
 }
 
 // Compensate the red channel of the image using (4).
-// Expects img in CV_64FC3 BGR colorspace.
-// Returns the compensated red channel.
-static Mat
-compensateRed(const Mat &img) {
+// Image is CV_64FC3 BGR.
+static void
+compensateRed(cv::Mat &img) {
+	assert(img.type() == CV_64FC3);
+
 	// Split BGR channels
-	Mat channels[3];
-	split(img, channels);
-	Mat &g = channels[1];
-	Mat &r = channels[2];
-
-	// Mean of green channel
-	Scalar gmeans = mean(g);
-	assert((255*(int)gmeans[1] | 255*(int)gmeans[2] | 255*(int)gmeans[3]) == 0); // should have only one channel
-	double gmean = gmeans[0];
-
-	// Mean of red channel
-	Scalar rmeans = mean(r);
-	assert((255*(int)rmeans[1] | 255*(int)rmeans[2] | 255*(int)rmeans[3]) == 0); // should have only one channel
-	double rmean = rmeans[0];
+	cv::Mat channels[3];
+	cv::split(img, channels);
+	cv::Mat &g = channels[1];
+	cv::Mat &r = channels[2];
 
 	// Compensate red channel
-	Mat tmp;
-	multiply(ALPHA * (gmean - rmean) * (1.0 - r), g, tmp);
+	cv::Mat tmp;
+	cv::multiply(ALPHA * (cv::mean(g)[0] - cv::mean(r)[0]) * (1.0 - r), g, tmp);
 	r = r + tmp;
 
-	return r;
+	// Merge channels
+	cv::merge(channels, 3, img);
 }
 
-static Mat
-enhance(const Mat &img) {
-	Mat r = compensateRed(img);
+// White-balance the image using Gray-World.
+// Image is CV_64FC3 BGR
+static void
+whiteBalance(cv::Mat &img) {
+	assert(img.type() == CV_64FC3);
+
+	// Convert to uint16
+	cv::normalize(img, img, UINT16_MAX, 0, cv::NORM_MINMAX);
+	img.convertTo(img, CV_16UC3);
+
+	cv::Ptr<cv::xphoto::GrayworldWB> wb = cv::xphoto::createGrayworldWB();
+	wb->balanceWhite(img, img);
+
+	// Convert back to float64
+	img.convertTo(img, CV_64FC3);
+	cv::normalize(img, img, 1.0, 0.0, cv::NORM_MINMAX);
+
+	writeImage("whitebalanced.png", img);
+}
+
+// Gamma-correct the white-balanced image to get the "first input" of the multiscale fusion.
+// Input and output are CV_64FC3 BGR.
+static cv::Mat
+gammaCorrect(const cv::Mat &img) {
+	assert(img.type() == CV_64FC3);
+
+	// Convert to uint8
+	cv::Mat g = img.clone();
+	cv::normalize(g, g, 255, 0, cv::NORM_MINMAX);
+	g.convertTo(g, CV_8UC3);
+
+	// Gamma correction
+	cv::Mat lookup(1, 256, CV_8U);
+	uchar *p = lookup.ptr();
+	for (int k = 0; k < 256; k++) {
+		p[k] = cv::saturate_cast<uchar>(pow(k / 255.0, GAMMA) * 255.0);
+	}
+	cv::LUT(g, lookup, g);
+
+	// Convert back to float64
+	g.convertTo(g, CV_64FC3);
+	cv::normalize(g, g, 1.0, 0.0, cv::NORM_MINMAX);
+
+	return g;
+}
+
+// Sharpen the white-balanced image using (6) to get the "second input" of the multiscale fusion.
+// Input and output are CV_64FC3 BGR.
+static cv::Mat
+sharpen(const cv::Mat &img) {
+	assert(img.type() == CV_64FC3);
+
+	cv::Mat blur, norm, s;
+
+	// Sharpen
+	cv::GaussianBlur(img, blur, GAUSS_KERN_SIZE, 0);
+	cv::normalize(img - blur, norm, 1.0, 0.0, cv::NORM_MINMAX);
+	s = (img + norm) / 2.0;
+
+	return s;
+}
+
+// Enhance the image using color balance and fusion.
+// Image is CV_64FC3 BGR.
+static void
+enhance(cv::Mat &img) {
+	assert(img.type() == CV_64FC3);
+
+	// Compensate red channel.
+	compensateRed(img);
+
+	// White balance with Gray-World
+	whiteBalance(img);
+
+	// Gamma correction and sharpening
+	cv::Mat g = gammaCorrect(img);
+	cv::Mat s = sharpen(img);
+	writeImage("gamma_corrected.png", g);
+	writeImage("sharpened.png", s);
 
 	// TODO
-	Mat enhanced;
-	return enhanced;
+
 }
 
 int
@@ -86,14 +161,16 @@ main(int argc, const char *argv[]) {
 		return 1;
 	}
 	const char *path = argv[1];
-	Mat img;
+	cv::Mat img;
 	Status status = readImage(path, img);
 	if (status != OK) {
 		cerr << "Error reading image " << path << endl;
 		return 1;
-	}
+	}	
 
-	Mat enhanced = enhance(img);
+	// Enhance image
+	enhance(img);
 
-	writeImage("enhanced.png", enhanced);
+	// Write output
+	writeImage("enhanced.png", img);
 }
